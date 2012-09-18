4929d285991e3c542a7168bd2d4c3cb35ceaa6db
Hans Loken <hans@ebamse.net>
2012-09-18 21:32:36 +0200

    Replaced ASPX style templating with Razor, implemented some glue and js viewmodels
    demo.js contains glue code + js view-models
    MvcApplication1 that used aspx style templating was replaced by
    MvcApplication that uses Razor

---
+++ Mvc4WithKnockoutAndTwitterBootstrap/MvcApplication/Scripts/demo.js
@@ -0,0 +1,84 @@
+﻿var demo = demo || {};
+
+$(function() {
+    demo.utils = function() {
+        var getPath = function(name) {
+            return "../Templates/HTML/_" + name + ".tmlp.html";
+        };
+
+        var renderTemplate = function(item) {
+            var file = getPath(item.name);
+            $.when($.get(file))
+                .done(function(tmplData) {
+                    $.templates({ tmpl: tmplData });
+                    item.selector($.render.tmpl(item.data));
+                });
+        };
+
+        return {
+            getPath: getPath,
+            renderTemplate: renderTemplate
+        };
+    }();
+});
+
+$(function() {
+    demo.head = function() {
+        var headHtml = ko.observable(),
+            loadTemplate = demo.utils.renderTemplate({
+                name: "head",
+                data: "",
+                selector: headHtml
+            });
+
+        return {
+            loadTemplate: loadTemplate,
+            headHtml: headHtml
+        };
+    }();
+
+    demo.body = function() {
+        var bodyHtml = ko.observable(),
+            loadTemplate = demo.utils.renderTemplate({
+                name: "home",
+                data: "",
+                selector: bodyHtml
+            });
+
+        return {
+            bodyHtml: bodyHtml,
+            loadTemplate: loadTemplate
+        };
+    }();
+
+    demo.contacts = function() {
+        var contactHtml = ko.observable(),
+            loadContacts = function() {
+                $.ajax({
+                    url: '/Home/LoadContacts',
+                    type: "POST",
+                    /*contentType: "application/json; charset=utf-8",*/
+                    dataType: 'json',
+                    success: function(d) {
+                        demo.utils.renderTemplate({
+                            path: "../",
+                            name: 'contacts',
+                            data: d,
+                            selector: contactHtml
+                        });
+                    }
+                });
+            };
+
+        return {
+            contactHtml: contactHtml,
+            loadContacts: loadContacts
+        };
+    }();
+
+    ko.applyBindings(demo.head);
+    ko.applyBindings(demo.body);
+
+    demo.contacts.loadContacts(); // Get data before binding template
+    ko.applyBindings(demo.contacts);
+})
---
+++ readme.txt
@@ -0,0 +1,2 @@
+Most (if not all) of this code is based on this article:
+http://www.codeproject.com/Articles/417821/Integrating-Knockout-JSRender-and-Tweeter-bootstra
